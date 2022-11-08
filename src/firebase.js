245a2084018f8910abbe3c0558fdede6a5c4de4e
ezico <greenportal0@gmail.com>
2022-11-08 03:14:38 +0100

    security update

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
+  authDomain: "wiftz-podcasts.firebaseapp.com",
+  projectId: "wiftz-podcasts",
+  storageBucket: "wiftz-podcasts.appspot.com",
+  messagingSenderId: "42188632923",
+  appId: "1:42188632923:web:fe111d1054c5850054e07e",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
