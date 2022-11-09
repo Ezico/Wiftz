e5730c56fdcba71d7f6ac22c31c6ba6263cc92b7
ezico <greenportal0@gmail.com>
2022-11-09 07:40:05 +0100

    protect routes

--- src/firebase.js
+++ src/firebase.js
@@ -2,10 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
-import { REACT_APP_FIREBASE_API_KEY } from "./utility/environment";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "wiftz-podcasts.firebaseapp.com",
   projectId: "wiftz-podcasts",
   storageBucket: "wiftz-podcasts.appspot.com",
