9d7e655c13aaba9d897521cd2a1042c0bee09feb
ezico <greenportal0@gmail.com>
2022-11-11 12:11:11 +0100

    new update

--- src/components/LatestPodcasts.jsx
+++ src/components/LatestPodcasts.jsx
@@ -27,77 +27,76 @@ const LatestPodcasts = ({ data, featured, bg }) => {
                 />
               </div>
               <div className="col-md-8 col-sm-12">
-                <div className="">
-                  <h3 className="podcast-title">{item?.title}</h3>
-                  <p className="podcast-desc">
-                    <div
-                      dangerouslySetInnerHTML={{
-                        __html: item?.shortDescription.substring(0, 210),
-                      }}
-                    />
-                  </p>
-                  <br />
-                  <button className="btn secondary featured">
-                    Listen or Watch on
-                  </button>
-                  <br />
-                  <div className="medias row">
-                    {item.sportify ? (
-                      <div className="col-md-4 col-sm-6 sm-thumbnail">
-                        <a href={item.sportify}>
-                          <img className="" src={spotify_small} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    {item.apple ? (
-                      <div className="col-md-4 col-sm-6  sm-thumbnail">
-                        <a href={item.apple}>
-                          <img className="" src={apple} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    {item.amazon ? (
-                      <div className="col-md-4 col-sm-6  sm-thumbnail">
-                        <a href={item.amazon}>
-                          <img className="" src={amazon} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    {item.google ? (
-                      <div className="col-md-4 col-sm-6  sm-thumbnail">
-                        <a href={item.google}>
-                          <img className="" src={google} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    {item.pod ? (
-                      <div className="col-md-4 col-sm-6  sm-thumbnail">
-                        <a href={item.pod}>
-                          <img className="" src={pod} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                    {item.youtube ? (
-                      <div className="col-md-4 col-sm-6  sm-thumbnail">
-                        <a href={item.youtube}>
-                          <img className="" src={youtube} alt="" />
-                        </a>
-                      </div>
-                    ) : (
-                      ""
-                    )}
-                  </div>
+                <h3 className="podcast-title">{item?.title}</h3>
+                <p className="podcast-desc">
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: item?.shortDescription.substring(0, 210),
+                    }}
+                  />
+                </p>
+                <br />
+                <button className="btn secondary featured">
+                  Listen or Watch on
+                </button>
+                <br />
+                <div className="medias row">
+                  {item.sportify ? (
+                    <div className="col-md-4 col-sm-6 sm-thumbnail">
+                      <a href={item.sportify}>
+                        <img className="" src={spotify_small} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.apple ? (
+                    <div className="col-md-4 col-sm-6  sm-thumbnail">
+                      <a href={item.apple}>
+                        <img className="" src={apple} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.amazon ? (
+                    <div className="col-md-4 col-sm-6  sm-thumbnail">
+                      <a href={item.amazon}>
+                        <img className="" src={amazon} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.google ? (
+                    <div className="col-md-4 col-sm-6  sm-thumbnail">
+                      <a href={item.google}>
+                        <img className="" src={google} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.pod ? (
+                    <div className="col-md-4 col-sm-6  sm-thumbnail">
+                      <a href={item.pod}>
+                        <img className="" src={pod} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {item.youtube ? (
+                    <div className="col-md-4 col-sm-6  sm-thumbnail">
+                      <a href={item.youtube}>
+                        <img className="" src={youtube} alt="" />
+                      </a>
+                    </div>
+                  ) : (
+                    ""
+                  )}
                 </div>
+
                 <br />
               </div>
             </>
